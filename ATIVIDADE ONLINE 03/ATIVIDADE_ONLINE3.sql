d191aa796b29b6e0baef1d54fe9197cd2aad904c
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-09-24 12:03:56 -0300

    Atividade online 3

---
+++ ATIVIDADE ONLINE 03/ATIVIDADE_ONLINE3.sql
@@ -0,0 +1,14 @@
+INSERT INTO Usuarios Values ('Danox', 'Senha123'); 
+INSERT INTO Classes Values ('Monge', 'Descrição do monge');
+INSERT INTO Habilidades Values ('Recuperar a vida');
+INSERT INTO Personagens Values ('BitBug',2,2);
+INSERT INTO ClassesHabilidades Values (2,2), (2,3)
+
+
+INSERT INTO Usuarios Values ('Aphonso', 'Senha1234'); 
+INSERT INTO Classes Values ('Barbaro', 'Descrição do barbaro');
+INSERT INTO Habilidades Values ('Lanca mortal'),('Escudo Supremo');
+INSERT INTO Personagens Values ('Deu Bug',1,1);
+INSERT INTO ClassesHabilidades Values (1,1), (1,2)
+
+
